71717c626eeeea0167b3d2ac3834a2a20b118d56
Gilson Alencar <gilson.alencar94@gmail.com>
2021-09-02 18:48:57 -0300

    Refactor ex.6 function name

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte-2/exercise6.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte-2/exercise6.js
@@ -1,10 +1,9 @@
-
-function summatory(number) {
-    let sum = 0;
-    for (let i = 1; i <= number; i++) {
-        sum += i;
-    }
-    return sum;
+function summation(number) {
+  let sum = 0;
+  for (let i = 1; i <= number; i++) {
+    sum += i;
+  }
+  return sum;
 }
 
-console.log(summatory(5));
+console.log(summation(5));
