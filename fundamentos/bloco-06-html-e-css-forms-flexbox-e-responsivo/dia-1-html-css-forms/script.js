4fdd84c3edd611a6002048b5092ca31aea6ad0dc
Gilson Alencar <gilson.alencar94@gmail.com>
2021-09-27 22:55:54 -0300

    Parte 2

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,1 +1,43 @@
 //gerar lista de estados
+
+const estados = [
+  'Acre',
+  'Alagoas',
+  'Amapá',
+  'Amazonas',
+  'Bahia',
+  'Ceará',
+  'Distrito Federal',
+  'Espírito Santo',
+  'Goiás',
+  'Maranhão',
+  'Mato Grosso',
+  'Mato Grosso do Sul',
+  'Minas Gerais',
+  'Pará',
+  'Paraíba',
+  'Paraná',
+  'Pernambuco',
+  'Piauí',
+  'Rio de Janeiro',
+  'Rio Grande do Norte',
+  'Rio Grande do Sul',
+  'Rondônia',
+  'Roraima',
+  'Santa Catarina',
+  'São Paulo',
+  'Sergipe',
+  'Tocantins',
+];
+
+const select = document.getElementById('estado');
+
+for (const estado of estados) {
+  // console.log(estado);
+  const opt = document.createElement('option');
+  opt.value = estado;
+  opt.text = estado;
+  select.appendChild(opt);
+}
+
+// document.getElementById('estado')
