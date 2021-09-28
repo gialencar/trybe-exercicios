fc16eee6ce3fd4f17e0b77dd6c37309ae1aa0219
Gilson Alencar <gilson.alencar94@gmail.com>
2021-09-28 15:12:01 -0300

    Parte 1 concluida

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/parte1.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/parte1.js
@@ -1,49 +1,65 @@
 const order = {
-    name: 'Rafael Andrade',
-    phoneNumber: '11-98763-1416',
-    address: {
-      street: 'Rua das Flores',
-      number: '389',
-      apartment: '701',
-    },
-    order: {
-      pizza: {
-        marguerita: {
-          amount: 1,
-          price: 25,
-        },
-        pepperoni: {
-          amount: 1,
-          price: 20,
-        }
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
       },
-      drinks: {
-        coke: {
-          type: 'Coca-Cola Zero',
-          price: 10,
-          amount: 1,
-        }
+      pepperoni: {
+        amount: 1,
+        price: 20,
       },
-      delivery: {
-        deliveryPerson: 'Ana Silveira',
-        price: 5,
-      }
     },
-    payment: {
-      total: 60,
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      },
     },
-  };
-  
-  const customerInfo = (order) => {
-    // Adicione abaixo as informações necessárias.
-  
-  }
-  
-  customerInfo(order);
-  
-  const orderModifier = (order) => {
-    // Adicione abaixo as informações necessárias.
-  
-  }
-  
-  orderModifier(order);
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    },
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+const customerInfo = (order) => {
+  // Adicione abaixo as informações necessárias.
+  const deliveryPerson = order.order.delivery.deliveryPerson;
+  const customer = order.name;
+  const tel = order.phoneNumber;
+  const street = order.address.street;
+  const number = order.address.number;
+  const apart = order.address.apartment;
+
+  console.log(
+    `Olá ${deliveryPerson}, entrega para ${customer}, Tel:${tel}, End:${street}, Nº${number}, AP:${apart}`
+  );
+};
+
+customerInfo(order);
+
+const orderModifier = (order) => {
+  // Adicione abaixo as informações necessárias.
+  const pizzas = Object.keys(order.order.pizza).join(', ');
+  const drink = order.order.drinks.coke.type;
+  const customer = (order.name = 'Luiz Silva');
+  const total = (order.payment.total = 50);
+
+  console.log(
+    `Olá ${customer}, o total do seu pedido de ${pizzas} e ${drink} é ${total}`
+  );
+};
+
+orderModifier(order);
