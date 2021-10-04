7e55ac230c0eec3a6622f3dbc279d95be0e84e5a
Gilson Alencar <gilson.alencar94@gmail.com>
2021-10-04 15:38:23 -0300

    Exercicio 1

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio1.js
@@ -0,0 +1,17 @@
+const newPerson = (name) => {
+  return {
+    name,
+    email: `${name.replace(/\s/g, '').toLowerCase()}@trybe.com`,
+  };
+};
+
+const newEmployees = () => {
+  const employees = {
+    id1: newPerson('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: newPerson('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: newPerson('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  };
+  return employees;
+};
+
+console.log(newEmployees());
