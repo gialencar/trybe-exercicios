93b4c3e7988a6573de90428b55c32c3eebf97884
Gilson Alencar <gilson.alencar94@gmail.com>
2021-10-06 18:29:26 -0300

    Exercise 5

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise5.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise5.js
@@ -62,3 +62,18 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+const expectedResult = [
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien',
+];
+
+function fantasyOrScienceFictionAuthors() {
+  return books.filter((book) => {
+    return book.genre === 'Ficção Científica' || book.genre === 'Fantasia';
+  }).map((book) => `${book.author.name}`).sort();
+
+}
+
+console.log(fantasyOrScienceFictionAuthors());
