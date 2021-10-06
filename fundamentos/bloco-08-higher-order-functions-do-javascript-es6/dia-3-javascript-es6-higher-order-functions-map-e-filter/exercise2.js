8e424e8b546427545c1beaa689586b59931d4508
Gilson Alencar <gilson.alencar94@gmail.com>
2021-10-06 16:05:47 -0300

    Exercise 2

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise2.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise2.js
@@ -62,3 +62,40 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+const expectedResult = [
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
+];
+
+function nameAndAge() {
+  return books.map((book) => {
+    return {
+      age: book.releaseYear - book.author.birthYear,
+      author: book.author.name,
+    };
+  }).sort((a, b) => a.age - b.age);
+}
+
+console.log(nameAndAge());
