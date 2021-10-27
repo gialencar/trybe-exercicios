6cedcc0d8d64c8d0009a1e682ac8dfce0048fbde
Gilson Alencar <gilson.alencar94@gmail.com>
2021-10-27 19:50:04 -0300

    feat: finaliza exercicios

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercise3/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercise3/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './header';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
   return (
@@ -8,14 +10,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Header />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Content />
+        <Footer />
       </header>
     </div>
   );
---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercise3/src/Content.jsx
@@ -0,0 +1,42 @@
+import { Component } from 'react';
+
+const conteudos = [
+  {
+    conteudo: 'High Order Functions',
+    bloco: 8,
+    status: 'Aprendido',
+  },
+  {
+    conteudo: 'Composicao de Componentes',
+    bloco: 11,
+    status: 'Aprendendo',
+  },
+  {
+    conteudo: 'Composicao de Estados',
+    bloco: 12,
+    status: 'Aprenderei',
+  },
+  {
+    conteudo: 'Redux',
+    bloco: 16,
+    status: 'Aprenderei',
+  },
+];
+
+class Content extends Component {
+  render() {
+    return (
+      <>
+        {conteudos.map((conteudo) => (
+          <div key={conteudo.conteudo} className='card'>
+            <h3>{`O conteudo é: ${conteudo.conteudo}`}</h3>
+            <p>{`Status: ${conteudo.status}`}</p>
+            <p>{`Bloco: ${conteudo.bloco}`}</p>
+          </div>
+        ))}
+      </>
+    );
+  }
+}
+
+export default Content;
