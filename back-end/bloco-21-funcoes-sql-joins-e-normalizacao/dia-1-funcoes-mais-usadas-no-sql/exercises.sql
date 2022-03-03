8f39db0807f95b426ee7a9fe270deb79386f1dc3
Gilson Alencar <gilson.alencar94@gmail.com>
2022-03-03 19:30:20 -0300

    exercises 4-7

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercises.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercises.sql
@@ -1,18 +1,40 @@
 use hr;
+
 -- 1. Escreva uma query que exiba o maior salário da tabela.
 SELECT MAX(SALARY) FROM employees;
+
 -- 2. Escreva uma query que exiba a diferença entre o maior e o menor salário.
 SELECT MAX(SALARY) - MIN(SALARY) FROM employees;
+
 -- 3. Escreva uma query que exiba a média salarial de cada JOB_ID , ordenando pela média salarial em ordem decrescente.
 SELECT 
     JOB_ID, AVG(SALARY) AS media_salarial
 FROM employees
 GROUP BY JOB_ID
 ORDER BY media_salarial DESC;
+
 -- 4. Escreva uma query que exiba a quantidade de dinheiro necessária para realizar o pagamento de todas as pessoas funcionárias.
+SELECT SUM(SALARY) FROM employees;
+
 -- 5. Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os salários e a média dos salários. Todos os valores devem ser formatados para ter apenas duas casas decimais.
+SELECT 
+    TRUNCATE(MAX(SALARY), 2),
+    TRUNCATE(MIN(SALARY), 2),
+    TRUNCATE(SUM(SALARY), 2),
+    TRUNCATE(AVG(SALARY), 2)
+FROM
+    employees;
+
 -- 6. Escreva uma query que exiba a quantidade de pessoas que trabalham como pessoas programadoras ( IT_PROG ).
+SELECT JOB_ID, COUNT(*)
+FROM employees
+WHERE JOB_ID = 'IT_PROG';
+
 -- 7. Escreva uma query que exiba a quantidade de dinheiro necessária para efetuar o pagamento de cada profissão ( JOB_ID ).
+SELECT JOB_ID, SUM(SALARY)
+FROM employees
+GROUP BY JOB_ID;
+
 -- 8. Utilizando a query anterior, faça as alterações para que seja exibido somente a quantidade de dinheiro necessária para cobrir a folha de pagamento das pessoas programadoras ( IT_PROG ).
 -- 9. Escreva uma query que exiba em ordem decrescente a média salarial de todos os cargos, exceto das pessoas programadoras ( IT_PROG ).
 -- 10. Escreva um query que exiba média salarial e o número de funcionários de todos os departamentos com mais de dez funcionários. Dica: agrupe pelo department_id .
