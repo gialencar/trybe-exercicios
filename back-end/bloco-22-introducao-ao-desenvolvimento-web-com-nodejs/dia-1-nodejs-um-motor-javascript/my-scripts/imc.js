503bfceddbc8a1f61b49a0b94e80981beb48d385
Gilson Alencar <gilson.alencar94@gmail.com>
2022-03-11 19:03:02 -0300

    feat: exercicio 5

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
@@ -4,7 +4,23 @@ const peso = readline.questionInt('Qual o seu peso? ');
 const altura = readline.questionFloat('Qual a sua altura? ');
 
 function imc(peso, altura) {
-  return peso / altura ** 2;
+  const imc = (peso / altura ** 2).toFixed(2);
+
+  if (imc < 18.5) {
+    return `IMC: ${imc} - Abaixo do peso (magreza)`;
+  }
+  if (imc >= 18.5 && imc < 25) {
+    return `IMC: ${imc} - Peso normal`;
+  }
+  if (imc >= 25 && imc < 30) {
+    return `IMC: ${imc} - Acima do peso (sobrepeso)`;
+  }
+  if (imc >= 30 && imc < 35) {
+    return `IMC: ${imc} - Obesidade grau I`;
+  }
+  if (imc >= 35 && imc < 40) {
+    return `IMC: ${imc} - Obesidade grau II`;
+  }
 };
 
-console.log(imc(peso, altura).toFixed(2));
+console.log(imc(peso, altura));
