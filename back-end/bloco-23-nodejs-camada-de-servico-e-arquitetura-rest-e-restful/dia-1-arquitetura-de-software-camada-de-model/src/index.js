e1cbdd9b5905967d0d5489357c2a337c91ac9f8d
Gilson Alencar <gilson.alencar94@gmail.com>
2022-03-24 19:22:13 -0300

    feat: create users router

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/src/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const router = require('./routes/users.route');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Ouvindo a porta ${PORT}`);
