dd8760ead927154c8c04e2898eeaba2fa803ab09
Gilson Alencar <gilson.alencar94@gmail.com>
2022-05-04 19:17:53 -0300

    exercicio 1

---
+++ back-end/bloco-25-deployment/dia-1-infraestrutura-deploy-com-heroku/api/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/check', function(req, res, next) {
+  res.status(200).json({message: 'Está vivo!!!'});
+});
+
+module.exports = router;
